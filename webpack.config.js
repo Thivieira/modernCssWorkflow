c989b7c99c2af038a6fe5a4eb0d69f802df96c24
Thiago Alves Vieira <thiago.a.vieira@hotmail.com>
2018-01-10 10:37:33 -0200

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,73 @@
+const path = require("path");
+const glob = require("glob");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const PurifyCSSPlugin = require("purifycss-webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+const buildPath = __dirname + "/dist";
+module.exports = {
+  entry: {
+    app: "./src/app.js"
+  },
+  output: {
+    filename: "[name].js",
+    path: buildPath
+  },
+  devtool: "cheap-module-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: ["css-loader", "postcss-loader"]
+        })
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin("[name].[contenthash].css"),
+    // Make sure this is after ExtractTextPlugin!
+    new PurifyCSSPlugin({
+      // Give paths to parse for rules. These should be absolute!
+      paths: glob.sync(path.join(__dirname, "src/*.html"))
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: "vendor",
+      filename: "vendor.[chunkhash].js",
+      minChunks(module) {
+        return module.context && module.context.indexOf("node_modules") >= 0;
+      }
+    }),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "src/index.html"),
+      path: buildPath,
+      excludeChunks: ["base"],
+      filename: "index.html",
+      minify: {
+        collapseWhitespace: true,
+        collapseInlineTagWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+        screw_ie8: true,
+        conditionals: true,
+        unused: true,
+        comparisons: true,
+        sequences: true,
+        dead_code: true,
+        evaluate: true,
+        if_return: true,
+        join_vars: true
+      },
+      output: {
+        comments: false
+      }
+    }),
+    new webpack.HashedModuleIdsPlugin()
+  ]
+};
